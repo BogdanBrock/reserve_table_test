7965f403e80da7bbc930f37dc1beb957bede9a14
Bogdan Brock <bogdan.brock@yandex.ru>
2025-04-10 00:28:44 +0300

    created db

---
+++ app/core/db.py
@@ -0,0 +1,33 @@
+"""Модуль для создания базовой модели и фабрики сессий."""
+
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (AsyncAttrs, AsyncSession,
+                                    async_sessionmaker, create_async_engine)
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+engine = create_async_engine(
+    'postgres+asyncpg://'
+    'postgres_user:postgres_password@'
+    'localhost:5432:postgres_db',
+    echo=True
+)
+
+async_session_maker = async_sessionmaker(engine,
+                                         expire_on_commit=False,
+                                         class_=AsyncSession)
+
+
+async def db_session() -> AsyncGenerator[AsyncSession, None]:
+    """Функция для создания сессий."""
+    with async_session_maker() as session:
+        yield session
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    """Базовая модель Base."""
+
+    __abstract__ = True
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
